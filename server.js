1bd9203b5aff36d18c51ff3a1c9fa1dec9205e93
Efrat Stoztki <efratvaik@gmail.com>
2022-12-12 13:48:53 +0200

    after build

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ const http = require("http").createServer(app);
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.resolve(__dirname, "public")));
-// } else {
-//   const corsOptions = {
-//     origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
-//     credentials: true,
-//   };
-//   app.use(cors(corsOptions));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "public")));
+} else {
+  const corsOptions = {
+    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+}
 
 const authRoutes = require("./api/auth/auth.routes");
 const userRoutes = require("./api/user/user.routes");
@@ -55,6 +55,6 @@ app.listen(port, () => {
 
 
 // const port = process.env.PORT || 3030;
-http.listen(port, () => {
-  logger.info("Server is running on port: " + port);
-});
+// http.listen(port, () => {
+//   logger.info("Server is running on port: " + port);
+// });
