eee8a933fef2b89bf5e63104941d575324205ec5
Efrat Stoztki <efratvaik@gmail.com>
2022-12-12 11:35:17 +0200

    render back

--- server.js
+++ server.js
@@ -10,15 +10,17 @@ const http = require("http").createServer(app);
 app.use(cookieParser());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "public")));
-} else {
-  const corsOptions = {
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
-    credentials: true,
-  };
-  app.use(cors(corsOptions));
-}
+app.use(express.static('public'))
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.resolve(__dirname, "public")));
+// } else {
+//   const corsOptions = {
+//     origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+//     credentials: true,
+//   };
+//   app.use(cors(corsOptions));
+// }
 
 const authRoutes = require("./api/auth/auth.routes");
 const userRoutes = require("./api/user/user.routes");
@@ -44,7 +46,15 @@ app.get("/**", (req, res) => {
 });
 
 const logger = require("./services/logger.service");
+
+
 const port = process.env.PORT || 3030;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`)
+});
+
+
+// const port = process.env.PORT || 3030;
 http.listen(port, () => {
   logger.info("Server is running on port: " + port);
 });
